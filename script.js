41a72d80079017aa68da0d72e278905195a2580a
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-03-12 20:33:26 +1100

    Edit js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const fortuneButton = document.getElementById('fetch-fortune-btn')
 const output = document.querySelector('.api-output')
 const loadingButton = document.getElementById("loadingButton")
 const outputContainer = document.getElementById("output-container")
+const emailButton = document.getElementById('fetch-email-btn')
 
 // Function for Fortune page
 if (fortuneButton) {
@@ -32,7 +33,53 @@ if (fortuneButton) {
                 if (response.ok) {
                     response.json().then((json) => {
                         output.textContent = json.choices[0].text.trim();
-                    });~
+                    });
+                }
+                
+                outputContainer.style.display = 'block';
+                loadingButton.style.display = 'none';
+                selectFeedbackForm.style.display = 'block';
+                outputContainer.scrollIntoView({ behavior: 'smooth' })
+            });
+    
+            // console.log("Completed!");
+    
+        });
+    }
+
+// Function for EmailGenie page
+if (emailButton) {
+    emailButton.addEventListener('click', async () => {
+        loadingButton.style.display = 'block'
+        loadingButton.scrollIntoView({ behavior: 'smooth' })
+        const getSelectedMember = document.querySelector('input[name="communityMembers"]:checked')
+        // console.log(getSelectedMember)
+        // console.log("Button clicked")
+        const text = document.getElementById("textareaspace").value
+        // console.log(text)
+        const prompt = `you are a helpful, email-writing bot with a professional tone. I say please write me a correctly formatted email from a teacher to a ${getSelectedMember.id} about the subject of: ${text}. You reply:`
+        // console.log(prompt)
+    
+        const keyresp = await fetch('/.netlify/functions/get-token')
+        .then(response => response.json()
+        )
+    
+        const response = await fetch(
+                `https://api.openai.com/v1/completions`,
+                {
+                    body: JSON.stringify({"model": "text-davinci-003", "prompt": prompt, "temperature": 0.86, "max_tokens": 200}),
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json",
+                        Authorization:`Bearer ` + keyresp['message'],
+                    },
+                        }
+            ).then((response) => {
+                console.log(text)
+                if (response.ok) {
+                    response.json().then((json) => {
+                        output.textContent = json.choices[0].text.trim();
+                    });
                 }
                 
                 outputContainer.style.display = 'block';
@@ -46,22 +93,4 @@ if (fortuneButton) {
         });
 }
 
-
-
-
-// Function to copy output to clipboard. N.B. User must enable copying in their browser
-function copyToClipboard() {
-    // Get the text field
-    var copyText = document.getElementById("myInput");
-
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value).then(function(x) {
-        // alert("Copied to clipboard: " + copyText.value);
-        alert("Copied to clipboard: ");
-    });
-}    
 
